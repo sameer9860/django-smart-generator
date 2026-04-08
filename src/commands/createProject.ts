c1dc452c21f8af6b0100d826c188f97d3375d3be
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-08 07:23:00 +0545

    create project

--- src/commands/createProject.ts
+++ src/commands/createProject.ts
@@ -1,5 +1,14 @@
 import * as vscode from 'vscode';
+import { showProjectWizard } from '../wizard/projectWizard';
 
 export async function createProject() {
-    vscode.window.showInformationMessage('Django: Create Smart Project command triggered!');
+    const config = await showProjectWizard();
+
+    if (config) {
+        vscode.window.showInformationMessage(
+            `Django Project: ${config.projectName} | App: ${config.appName} | Env: ${config.envName} | .env: ${config.createEnvFiles}`
+        );
+    } else {
+        vscode.window.showWarningMessage('Django project creation cancelled.');
+    }
 }
